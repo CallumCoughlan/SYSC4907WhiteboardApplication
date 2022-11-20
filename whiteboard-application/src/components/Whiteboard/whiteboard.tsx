4cba429f54f21b5d44bcdabbf492ed7f97204ba6
CallumCoughlan <callum.d.w.coughlan@gmail.com>
2022-11-20 13:54:18 -0500

    intialize fabric.js canvas

--- whiteboard-application/src/components/Whiteboard/whiteboard.tsx
+++ whiteboard-application/src/components/Whiteboard/whiteboard.tsx
@@ -1,74 +1,61 @@
 import React, { useEffect } from 'react';
+import { fabric } from "fabric"
 import "./style.css"
+import { Color } from 'fabric/fabric-impl';
 
 interface WhiteboardProps {
     tool: string
 }
 
-function Whiteboard(props : WhiteboardProps) {
+const className = 'drawing-board';
+
+function Whiteboard(props: WhiteboardProps) {
 
     useEffect(() => {
-        if (props.tool === "pencil") {
-            console.log("pencil");
-            drawWithMouse();
-        } else if (props.tool === "rectangle") {
-            console.log("rectangle");
-        } else if (props.tool === "circle") {
-            console.log("circle");
-        }
-    });
+        const whiteboard = new fabric.Canvas(className, { isDrawingMode: true });
+        const ctx = whiteboard.getContext();
 
-    function drawWithMouse() {
-        var canvas = document.getElementById('drawing-board') as HTMLCanvasElement;
-        var context = canvas.getContext('2d');
+        fabric.Object.prototype.transparentCorners = false;
 
-        var canvasDiv = document.getElementById('canvas-style');
-        var canvasDivStyle = getComputedStyle(canvasDiv!);
-        canvas.width = parseInt(canvasDivStyle.getPropertyValue('width'));
-        canvas.height = parseInt(canvasDivStyle.getPropertyValue('height'));
+        const drawingMode = document.getElementById('drawing-mode') as HTMLCanvasElement,
+            lineColor = 'red', //document.getElementById('drawing-color') as HTMLCanvasElement,
+            lineWidth = 5, //document.getElementById('drawing-line-width') as HTMLElement,
+            clear = document.getElementById('clear-canvas') as HTMLCanvasElement;
 
-        //line settings for what the line looks like when drawn with mouse
-        context!.lineWidth = 3;
-        context!.lineJoin = 'bevel';
-        context!.lineCap = 'round';
-        context!.strokeStyle = 'black';
+        clear.onclick = function() { whiteboard.clear() };
 
-        //current and previous mouse positions for begining and end of line
-        var mouseLocation = {x: 0, y: 0};
-        var previousMouseLocation = {x: 0, y: 0};
-
-        //when the mouse moves this changes the current and old mouse locations
-        canvas.addEventListener('mousemove', function(e) {
-            previousMouseLocation.x = mouseLocation.x;
-            previousMouseLocation.y = mouseLocation.y;
-
-            mouseLocation.x = e.pageX - this.offsetLeft;
-            mouseLocation.y = e.pageY - this.offsetTop;
-        }, false);
-
-        //When the user holds the mouse button down it starts the listener and paints
-        canvas.addEventListener('mousedown', function(e) {
-            canvas.addEventListener('mousemove', onPaint, false);
-        }, false);
-
-        //when the user releases the mouse button it stops the listener and does not paint
-        canvas.addEventListener('mouseup', function() {
-            canvas.removeEventListener('mousemove', onPaint, false);
-        }, false);
-
-        //when the mousemove eventListener is active this function handles the drawing
-        var onPaint = function() {
-            context!.beginPath();
-            context!.moveTo(previousMouseLocation.x, previousMouseLocation.y);
-            context!.lineTo(mouseLocation.x, mouseLocation.y);
-            context!.closePath();
-            context!.stroke();
+        drawingMode.onclick = function() {
+            whiteboard.isDrawingMode = !whiteboard.isDrawingMode;
+            if (whiteboard.isDrawingMode) {
+                drawingMode.innerHTML = 'Selector Mode';
+            }
+            else {
+                drawingMode.innerHTML = 'Drawing Mode';
+            }
         };
-    }
+
+        if (whiteboard.freeDrawingBrush) {
+            whiteboard.freeDrawingBrush.color = lineColor;
+            whiteboard.freeDrawingBrush.width = lineWidth;
+        }
+
+        /*
+        lineColor.onchange = function() {
+            let brush = whiteboard.freeDrawingBrush;
+            brush.color = lineColor;
+        };
+        */
+
+        /*
+        document.getElementById('tool-selector').onchange = function() {
+
+        }
+        */
+    });
 
     return (
         <div className='canvas-style' id='canvas-style'>
-            <canvas className='drawing-board' id="drawing-board"></canvas>
+                <canvas className={className} id={className}></canvas>
         </div>
     );
 }
