e598b386f058c7471877c36823f83e45b22fadf3
Ryan Nguyen <ryannguyen3@cmail.carleton.ca>
2023-01-15 15:16:20 -0500

    Path Navigation and Login Page

--- whiteboard-application/src/components/pages/Whiteboard/whiteboardLayout.tsx
+++ whiteboard-application/src/components/pages/Whiteboard/whiteboardLayout.tsx
@@ -3,11 +3,31 @@ import { FC } from 'react';
 import { FabricCanvasContainer } from '../../../containers';
 import { Whiteboard, Toolbar } from '../../Whiteboard';
 
+import LoginForm from './loginForm';
+import RegisterForm from './registrationForm';
+import { BrowserRouter as Router, Routes, Route,} from "react-router-dom";
+
+
 const WhiteboardLayout: FC = () => (
-  <FabricCanvasContainer.Provider>
-    <Toolbar />
-    <Whiteboard />
-  </FabricCanvasContainer.Provider>
+  // <FabricCanvasContainer.Provider>
+  //   <Toolbar />
+  //   <Whiteboard />
+  // </FabricCanvasContainer.Provider>
+  <Router>
+    <div className="App">
+        <div className="container d-flex align-items-center flex-column">
+          <Routes>
+            <Route path="/" element={<LoginForm/>}/>
+            <Route path="/register" element={<RegisterForm/>}/>
+            <Route path="/whiteboard" element={
+              <FabricCanvasContainer.Provider>
+                <Toolbar />
+                <Whiteboard />
+              </FabricCanvasContainer.Provider>}/>
+          </Routes>
+        </div>
+    </div>
+    </Router>
 );
 
 export default WhiteboardLayout;
