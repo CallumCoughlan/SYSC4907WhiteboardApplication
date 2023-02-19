a919b57b63a6e6ba5b8e0bce5b6b9e5b55fb6778
Aleksandar Veselinovic <vesa.2k@gmail.com>
2023-02-18 19:10:54 -0500

    Add set availability page + form updates

--- whiteboard-application/src/components/pages/Scheduler/requestSession.tsx
+++ whiteboard-application/src/components/pages/Scheduler/requestSession.tsx
@@ -13,6 +13,26 @@ const RequestSession: FC<RequestSessionProps> = (props) => {
     const [endTime, setEndTime] = useState("");
     const [course, setCourse] = useState("");
 
+    function isTimeInvalid(time: String) {
+        if (startTime === "") {
+            return false
+        } else {
+            const startTimeValues = startTime.split(":")
+            const values = time.split(":")
+            if ((parseInt(startTimeValues[0]) * 60 + parseInt(startTimeValues[1])) >= (parseInt(values[0]) * 60 + parseInt(values[1]))) {
+                return true
+            } else {
+                return false
+            }
+        }
+    }
+
+    function handleStartTimeChange(event: React.ChangeEvent<HTMLSelectElement>) {
+        setStartTime(event.target.value)
+        var select = document.getElementById("end-time-select")
+        select?.removeAttribute("disabled")
+    }
+
     return (
         <form>
             <h1 className='form-title'>{props.role === "student" ? "Request a Session" : "Create a Session"}</h1>
@@ -21,7 +41,7 @@ const RequestSession: FC<RequestSessionProps> = (props) => {
                 <input type="date" id="date-input" value={date} className="form-item" onChange={(e)=>setDate(e.target.value)}/>
                 <br/>
                 <label htmlFor="start-time-select">Select a start time:</label>
-                <select value={startTime} id="start-time-select" className="form-item" onChange={(e)=>setStartTime(e.target.value)}>
+                <select value={startTime} id="start-time-select" className="form-item" onChange={(e)=>handleStartTimeChange(e)}>
                     <option value="8:30">8:30</option>
                     <option value="9:00">9:00</option>
                     <option value="9:30">9:30</option>
@@ -40,22 +60,22 @@ const RequestSession: FC<RequestSessionProps> = (props) => {
                 </select>
                 <br/>
                 <label htmlFor="end-time-select">Select an end time:</label>
-                <select value={endTime} id="end-time-select" className="form-item" onChange={(e)=>setEndTime(e.target.value)}>
-                    <option value="9:00">9:00</option>
-                    <option value="9:30">9:30</option>
-                    <option value="10:00">10:00</option>
-                    <option value="10:30">10:30</option>
-                    <option value="11:00">11:00</option>
-                    <option value="11:30">11:30</option>
-                    <option value="12:00">12:00</option>
-                    <option value="12:30">12:30</option>
-                    <option value="13:00">13:00</option>
-                    <option value="13:30">13:30</option>
-                    <option value="14:00">14:00</option>
-                    <option value="14:30">14:30</option>
-                    <option value="15:00">15:00</option>
-                    <option value="15:30">15:30</option>
-                    <option value="16:00">16:00</option>
+                <select value={endTime} id="end-time-select" className="form-item" onChange={(e)=>setEndTime(e.target.value)} disabled>
+                    <option value="9:00" disabled={isTimeInvalid("9:00")}>9:00</option>
+                    <option value="9:30" disabled={isTimeInvalid("9:30")}>9:30</option>
+                    <option value="10:00" disabled={isTimeInvalid("10:00")}>10:00</option>
+                    <option value="10:30" disabled={isTimeInvalid("10:30")}>10:30</option>
+                    <option value="11:00" disabled={isTimeInvalid("11:00")}>11:00</option>
+                    <option value="11:30" disabled={isTimeInvalid("11:30")}>11:30</option>
+                    <option value="12:00" disabled={isTimeInvalid("12:00")}>12:00</option>
+                    <option value="12:30" disabled={isTimeInvalid("12:30")}>12:30</option>
+                    <option value="13:00" disabled={isTimeInvalid("13:00")}>13:00</option>
+                    <option value="13:30" disabled={isTimeInvalid("13:30")}>13:30</option>
+                    <option value="14:00" disabled={isTimeInvalid("14:00")}>14:00</option>
+                    <option value="14:30" disabled={isTimeInvalid("14:30")}>14:30</option>
+                    <option value="15:00" disabled={isTimeInvalid("15:00")}>15:00</option>
+                    <option value="15:30" disabled={isTimeInvalid("15:30")}>15:30</option>
+                    <option value="16:00" disabled={isTimeInvalid("16:00")} selected>16:00</option>
                 </select>
                 <br/>
                 <label htmlFor="course-select">Select a course:</label>
--- whiteboard-application/src/components/pages/Whiteboard/whiteboardLayout.tsx
+++ whiteboard-application/src/components/pages/Whiteboard/whiteboardLayout.tsx
@@ -13,6 +13,7 @@ import AdminStudentList from './adminStudentList';
 import { BrowserRouter as Router, Routes, Route,} from "react-router-dom";
 import Calendar from '../../Calendar/calendar';
 import RequestSession from '../Scheduler/requestSession';
+import SetAvailability from '../Scheduler/setAvailability';
 
 
 const WhiteboardLayout: FC = () => (
@@ -25,6 +26,7 @@ const WhiteboardLayout: FC = () => (
             <Route path='/home' element={<Calendar role="scholar"/>}/>
             <Route path='/request-session' element={<RequestSession role="student"/>}/>
             <Route path='/create-session' element={<RequestSession role="scholar"/>}/>
+            <Route path='/set-availability' element={<SetAvailability/>}/>
             <Route path="/adminPage" element={<AdminForm/>}/>
             <Route path="/adminAssignSessions" element={<AdminAssignSess/>}/>
             <Route path="/adminScholarList" element={<AdminScholarList/>}/>
