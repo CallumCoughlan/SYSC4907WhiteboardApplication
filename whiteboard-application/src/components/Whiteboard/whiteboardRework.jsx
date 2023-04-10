f6238fad5917d5b7c2f6621527b2b5ee67909dba
CallumCoughlan <callum.d.w.coughlan@gmail.com>
2023-04-10 00:09:54 -0400

    Finalize Whiteboard rework

--- whiteboard-application/src/components/Whiteboard/whiteboardRework.jsx
+++ whiteboard-application/src/components/Whiteboard/whiteboardRework.jsx
@@ -23,7 +23,7 @@ const WhiteboardRework = () => {
 
     if (!editor.canvas.__eventListeners["mouse:down"]) {
       editor.canvas.on("mouse:down", function (opt) {
-        var evt = opt.e;
+        let evt = opt.e;
         if (evt.ctrlKey === true) {
           this.isDragging = true;
           this.selection = false;
@@ -36,8 +36,8 @@ const WhiteboardRework = () => {
     if (!editor.canvas.__eventListeners["mouse:move"]) {
       editor.canvas.on("mouse:move", function (opt) {
         if (this.isDragging) {
-          var e = opt.e;
-          var vpt = this.viewportTransform;
+          let e = opt.e;
+          let vpt = this.viewportTransform;
           vpt[4] += e.clientX - this.lastPosX;
           vpt[5] += e.clientY - this.lastPosY;
           this.requestRenderAll();
@@ -84,8 +84,35 @@ const WhiteboardRework = () => {
     editor.setStrokeColor(color);
   }, [color]);
 
+  const toggleCursor = () => {
+    editor.canvas.off('mouse:down').off('mouse:move').off('mouse:up');
+    editor.canvas.isDrawingMode = false;
+    
+    const allObjects = editor.canvas.getObjects();
+      allObjects.forEach((object) => {
+        object.selectable = true
+      });
+
+    addCursorListeners()
+  };
+
+  function addCursorListeners() {
+      editor.canvas.on("mouse:up", function (opt) {
+        editor.canvas.renderAll();
+        const json = editor.canvas.toJSON();
+        socket.emit("whiteboard-data", json);
+        this.selection = true;
+      });
+  }
+
   const toggleDraw = () => {
-    editor.canvas.isDrawingMode = !editor.canvas.isDrawingMode;
+    editor.canvas.off('mouse:down').off('mouse:move').off('mouse:up');
+    editor.canvas.isDrawingMode = true;
+
+    const allObjects = editor.canvas.getObjects();
+      allObjects.forEach((object) => {
+        object.selectable = true
+      });
   };
 
   const undo = () => {
@@ -119,30 +146,256 @@ const WhiteboardRework = () => {
     socket.emit("whiteboard-data", json);
   };
 
-  const onAddCircle = () => {
-    let circ = new fabric.Circle({radius: 50,
-      fill: '',
-      stroke: editor.canvas.freeDrawingBrush.color,
-      strokeWidth: editor.canvas.freeDrawingBrush.width,
-      top: 100, 
-      left: 100 
+  const onAddLine = () => {
+    //remove any previous listeners
+    editor.canvas.off('mouse:down').off('mouse:move').off('mouse:up');
+
+    //re-enable object selection
+    const allObjects = editor.canvas.getObjects();
+    allObjects.forEach((object) => {
+      object.selectable = true
     });
+
+    //get out of drawing mode
+    editor.canvas.isDrawingMode = false;
+
+    //add line mouse listeners
+    addLineMouseListeners()
+  }
+
+  function addLineMouseListeners() {
+    let line;
+    let isDown = false;
+    let origX = 0;
+    let origY = 0;
+  
+    editor.canvas.on('mouse:down', function (o) {
+      //temporarily disable object selection
+      const allObjects = editor.canvas.getObjects();
+      allObjects.forEach((object) => {
+        object.selectable = false
+      });
+      editor.canvas.selection = false;
+  
+      isDown = true;
+      const pointer = editor.canvas.getPointer(o.e);
+      origX = pointer.x;
+      origY = pointer.y;
+      line = new fabric.Line([origX,origY,origX,origY],{
+        stroke: editor.canvas.freeDrawingBrush.color,
+        strokeWidth: editor.canvas.freeDrawingBrush.width,
+      });
+  
+      editor.canvas.add(line);
+    });
+  
+    editor.canvas.on('mouse:move', function (o) { 
+      if (!isDown) return;
+      const pointer = editor.canvas.getPointer(o.e);
+  
+      line.set({
+        x2: pointer.x,
+        y2: pointer.y
+      })
+      editor.canvas.renderAll();
+    });
+  
+    editor.canvas.on('mouse:up', function (o) {
+      isDown = false;
+      //state.canvas.discardActiveObject();
+      editor.canvas.selection = true;
+  
+      const json = editor.canvas.toJSON();
+      socket.emit("whiteboard-data", json);
+    });
+  }
+
+  const onAddCircle = () => {
+      //remove any previous listeners
+      editor.canvas.off('mouse:down').off('mouse:move').off('mouse:up');
+
+      //re-enable object selection
+      const allObjects = editor.canvas.getObjects();
+      allObjects.forEach((object) => {
+        object.selectable = true
+      });
+
+      //get out of drawing mode
+      editor.canvas.isDrawingMode = false;
+
+      //add circle mouse listeners
+      addCircleMouseListeners()
+  };
+
+  // adds mouse listeners to canvas that add circles
+// state contains width and color for circles
+function addCircleMouseListeners() {
+  let circ;
+  let isDown = false;
+  let origX = 0;
+  let origY = 0;
+
+  editor.canvas.on('mouse:down', function (o) {
+    //temporarily disable object selection
+    const allObjects = editor.canvas.getObjects();
+    allObjects.forEach((object) => {
+      object.selectable = false
+    });
+
+    isDown = true;
+    const pointer = editor.canvas.getPointer(o.e);
+    origX = pointer.x;
+    origY = pointer.y;
+
+    circ = new fabric.Ellipse({
+      top: origY,
+      left: origX,
+      rx: 1,
+      ry: 1,
+      strokeWidth: editor.canvas.freeDrawingBrush.width,
+      stroke: editor.canvas.freeDrawingBrush.color,
+      fill: 'white'
+    });
+
     editor.canvas.add(circ);
+  });
+
+  editor.canvas.on('mouse:move', function (o) {
+    if (!isDown) return;
+    const pointer = editor.canvas.getPointer(o.e);
+
+    if (origX > pointer.x) {
+      circ.set({ left: Math.abs(pointer.x) });
+    }
+    if (origY > pointer.y) {
+      circ.set({ top: Math.abs(pointer.y) });
+    }
+
+    circ.set({ rx: Math.abs(origX - pointer.x) / 2 });
+    circ.set({ ry: Math.abs(origY - pointer.y) / 2 });
+
+    editor.canvas.renderAll();
+  });
+
+  editor.canvas.on('mouse:up', function (o) {
+    isDown = false;
+    // state.canvas.discardActiveObject();
+
+    editor.canvas.discardActiveObject();
+    //state.canvas.selection = true;
+
     const json = editor.canvas.toJSON();
     socket.emit("whiteboard-data", json);
+  });
+}
+
+  const onAddArrow = () => {
+    let triangle = new fabric.Triangle({
+      width: 10, 
+      height: 15, 
+      fill: editor.canvas.freeDrawingBrush.color, 
+      left: 235, 
+      top: 65,
+      angle: 90
+    });
+
+    let line = new fabric.Line([50, 100, 200, 100], {
+        left: 75,
+        top: 70,
+        stroke: editor.canvas.freeDrawingBrush.color
+    });
+
+    let objs = [line, triangle];
+
+    let alltogetherObj = new fabric.Group(objs);
+    editor.canvas.add(alltogetherObj);
   };
+
   const onAddRectangle = () => {
-    editor.addRectangle();
-    const json = editor.canvas.toJSON();
-    socket.emit("whiteboard-data", json);
+
+    editor.canvas.off('mouse:down').off('mouse:move').off('mouse:up');
+
+      //re-enable object selection
+      const allObjects = editor.canvas.getObjects();
+      allObjects.forEach((object) => {
+        object.selectable = true
+      });
+
+      //get out of drawing mode
+      editor.canvas.isDrawingMode = false;
+
+      //add rectangle mouse listeners
+      addRectangleMouseListeners()
   };
+
+  function addRectangleMouseListeners() {
+    let rect;
+    let isDown = false;
+    let origX = 0;
+    let origY = 0;
+  
+    editor.canvas.on('mouse:down', function (o) {
+      //temporarily disable object selection
+      const allObjects = editor.canvas.getObjects();
+      allObjects.forEach((object) => {
+        object.selectable = false
+      });
+  
+      isDown = true;
+      const pointer = editor.canvas.getPointer(o.e);
+      origX = pointer.x;
+      origY = pointer.y;
+  
+      rect = new fabric.Rect({
+        left: origX,
+        top: origY,
+        originX: 'left',
+        originY: 'top',
+        width: pointer.x - origX,
+        height: pointer.y - origY,
+        angle: 0,
+        fill: 'rgba(0,0,0,0)',
+        stroke: editor.canvas.freeDrawingBrush.color,
+        strokeWidth: editor.canvas.freeDrawingBrush.width,
+        transparentCorners: false,
+        selectable: true
+      });
+  
+      editor.canvas.add(rect);
+    });
+  
+    editor.canvas.on('mouse:move', function (o) {  
+      if (!isDown) return;
+      const pointer = editor.canvas.getPointer(o.e);
+  
+      if (origX > pointer.x) {
+        rect.set({ left: Math.abs(pointer.x) });
+      }
+      if (origY > pointer.y) {
+        rect.set({ top: Math.abs(pointer.y) });
+      }
+  
+      rect.set({ width: Math.abs(origX - pointer.x) });
+      rect.set({ height: Math.abs(origY - pointer.y) });
+  
+      editor.canvas.renderAll();
+    });
+  
+    editor.canvas.on('mouse:up', function (o) {
+      isDown = false;
+      editor.canvas.discardActiveObject();
+      const json = editor.canvas.toJSON();
+      socket.emit("whiteboard-data", json);
+    });
+  }
+
   const addText = () => {
     const textbox = new fabric.Textbox(
       "Insert Text", {
         selectable: true,
         evented: true,
         editable: true,
-        width: 100,
+        width: 200,
         height: 400
       }
     );
@@ -156,6 +409,17 @@ const WhiteboardRework = () => {
       return;
     }
     editor.canvas.freeDrawingBrush.width = width;
+    if (editor.canvas.getActiveObjects() != null) {
+      let objs = editor.canvas.getActiveObjects();
+
+      objs.forEach((object) => {
+        object.strokeWidth = width;
+      });  
+
+      editor.canvas.renderAll();
+      const json = editor.canvas.toJSON();
+      socket.emit("whiteboard-data", json);
+    }
   }, [width]);
 
   return (
@@ -165,14 +429,20 @@ const WhiteboardRework = () => {
             color="primary"
             aria-label="Platform"
         >
+          <Button onClick={toggleCursor}>
+            Cursor
+          </Button>
           <Button onClick={toggleDraw}>
-            Cursor/Draw
+            Draw
+          </Button>
+          <Button onClick={onAddLine}>
+            Line
           </Button>
           <Button onClick={onAddCircle}>
             Circle
           </Button>
-          <Button onClick={onAddRectangle}>
-            Rectangle
+          <Button onClick={onAddArrow}>
+            Arrow
           </Button>
           <Button onClick={onAddRectangle}>
             Rectangle
