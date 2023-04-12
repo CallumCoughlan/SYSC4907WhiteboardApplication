47b0c14834cd9d208324d92e8006a730e08f8eaa
CallumCoughlan <callum.d.w.coughlan@gmail.com>
2023-04-12 17:07:53 -0400

    Remove old whiteboard

--- whiteboard-application/src/components/Whiteboard/index.tsx
+++ whiteboard-application/src/components/Whiteboard/index.tsx
@@ -1,4 +1,2 @@
-export { default as Whiteboard } from "./whiteboard";
-export { default as Toolbar } from "./toolbar";
 export { default as MessageWindow } from "./messageWindow";
 export { default as AdminNavBar } from "./navBar";
--- whiteboard-application/src/components/pages/Whiteboard/whiteboardLayout.tsx
+++ whiteboard-application/src/components/pages/Whiteboard/whiteboardLayout.tsx
@@ -1,8 +1,6 @@
 import { FC } from 'react';
 
 import Box from '@mui/material/Box';
-import { FabricCanvasContainer } from '../../../containers';
-import { Whiteboard, Toolbar, MessageWindow } from '../../Whiteboard';
 
 import LoginForm from './loginForm';
 import RegisterForm from './registrationForm';
@@ -15,6 +13,7 @@ import Calendar from '../../Calendar/calendar';
 import RequestSession from '../Scheduler/requestSession';
 import SetAvailability from '../Scheduler/setAvailability';
 import WhiteboardRework from '../../Whiteboard/whiteboardRework';
+import { MessageWindow } from '../../Whiteboard';
 
 
 const WhiteboardLayout: FC = () => (
@@ -33,12 +32,10 @@ const WhiteboardLayout: FC = () => (
             <Route path="/adminScholarList" element={<AdminScholarList/>}/>
             <Route path="/adminStudentList" element={<AdminStudentList/>}/>
             <Route path="/whiteboard" element={
-              <FabricCanvasContainer.Provider>
-                <Box display="flex">
-                  <WhiteboardRework />
-                  <MessageWindow />
-                </Box>
-              </FabricCanvasContainer.Provider>}/>
+              <Box display="flex">
+                <WhiteboardRework />
+                <MessageWindow />
+              </Box>}/>
           </Routes>
         </div>
     </div>
